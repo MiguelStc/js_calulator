90c13086d6b867b4f541567a9aec0e93ffb64e35
Miguel Stc <104473270+MiguelStc@users.noreply.github.com>
2024-02-20 21:26:54 +0100

    +1

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // calculator
-
-
+//get what's displayed
 const display = document.getElementById("display");
 
-function appendToDisplay( input){
+// add what's clicked to display screen
+function appendToDisplay(input){
     display.value += input;
 
 }
-
+// empty display top
 function clearDisplay(){
     display.value="";
 }
 
+
+// calculate the values on screen
 function calculate(){
 
     try{
